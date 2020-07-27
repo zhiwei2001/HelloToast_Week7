5dcaf6bf4a1d54c1c27b45366e1b76d11be2f017
ASUS <limzw-wm19@student.tarc.edu.my>
2020-07-27 17:17:25 +0800

    homework commit by zhi wei

---
+++ app/src/main/java/com/example/hellotoast/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.example.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent=getIntent();
+        String message=intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textview=findViewById(R.id.textView2);
+        textview.setText(message);
+    }
+}
